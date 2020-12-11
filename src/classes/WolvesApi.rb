b54fa3a8528e112d73f46f1c856724d8621d0830
codefactor-io <support@codefactor.io>
2020-12-11 12:20:51 +0000

    [CodeFactor] Apply fixes

--- src/classes/WolvesApi.rb
+++ src/classes/WolvesApi.rb
@@ -1,6 +1,6 @@
 class WolvesApi
-  attr_accessor :status  
-    
+  attr_accessor :status
+
   def initialize(username)
     response = HTTParty.get("https://api.github.com/users/#{username}")
     @status = response.code
@@ -13,12 +13,12 @@ class WolvesInfos < WolvesApi
     if(@status == 200)
       pars = ["name", "bio", "followers", "following"]
       results = []
-      
+
       pars.each { |cont| results.push(@parser[cont]) }
 
       return """#{results[0].to_s.rjust(8," ")} ~ #{results[1]}
       #   #{results[2].to_s.rjust(8," ")}  Followers
-      #   #{results[3].to_s.rjust(8," ")}  Following""" 
+      #   #{results[3].to_s.rjust(8," ")}  Following"""
     else
       return @status
     end
